36f546ab171f76c2c4515e3df1d4051b23040743
Alexa <strekalo@gmail.com>
2011-11-28 14:58:10 +0100

    first commit

---
+++ src/main/java/de/gwzberlin/zas/survey/App.java
@@ -0,0 +1,13 @@
+package de.gwzberlin.zas.survey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
